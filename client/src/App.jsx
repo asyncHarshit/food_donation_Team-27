291371c85265771335444d6a678202f6de680098
Ishika Rathore <ishikarathore030@gmai.com>
2025-12-04 13:19:38 +0530

    feat:switch app.jsx to tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
 // src/App.jsx
-import {  Routes, Route } from "react-router-dom";
-import DonorDashboard from "./pages/DonorDashboard";
-import VolunteerDashboard from "./pages/VolunteerDashboard";
-import LandingPage from "./pages/LandingPage";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import LandingPage from "./pages/LandingPage";
+import AdminDashboard from "./pages/AdminDashboard";
 
 export default function App() {
   return (
-
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/donor" element={<DonorDashboard />} />
-        <Route path="/volunteer" element={<VolunteerDashboard />} />
-        {/* <Route path="/admin" element={<AdminLayout />} /> */}
-      </Routes>
-  
+    <BrowserRouter>
+      <div className="min-h-screen bg-slate-50 text-slate-900">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/donor" element={<DonorDashboard />} />
+          <Route path="/volunteer" element={<VolunteerDashboard />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
