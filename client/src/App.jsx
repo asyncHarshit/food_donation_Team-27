411fe10de360cce61cfa3a4f1fe016aef4a71d11
harshit <harshitkumarsingh203@gmail.com>
2025-12-04 12:45:19 +0530

    feat: Add Firebase configuration and integrate react-router-dom for routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,19 @@
 // src/App.jsx
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage.jsx";
-import DonorDashboard from "./pages/DonorDashboard.jsx";
-import VolunteerDashboard from "./pages/VolunteerDashboard.jsx";
-import AdminLayout from "./pages/AdminLayout.jsx";
+import {  Routes, Route } from "react-router-dom";
+import DonorDashboard from "./pages/DonorDashboard";
+import VolunteerDashboard from "./pages/VolunteerDashboard";
+import LandingPage from "./pages/LandingPage";
+
 
 export default function App() {
   return (
-    <BrowserRouter>
+
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/donor" element={<DonorDashboard />} />
         <Route path="/volunteer" element={<VolunteerDashboard />} />
-        <Route path="/admin" element={<AdminLayout />} />
+        {/* <Route path="/admin" element={<AdminLayout />} /> */}
       </Routes>
-    </BrowserRouter>
+  
   );
 }
